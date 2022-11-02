9cdab39af5c7138daf0a9c0485b7e41d223908d6
zakissimo <zakaria.habri@gmail.com>
2022-11-02 16:50:18 +0100

    up

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 07:58:03 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/02 11:53:23 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/02 15:50:10 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,35 @@
 #include "libft/libft.h"
 #include "mlx/mlx.h"
 
-int		WIDTH = 500;
-int		LENGTH = 500;
-
-void	ft_exit(int ret, char *msg)
+void	destroy_and_free(void *mlx_id, void *mlx_win)
 {
-	if (msg)
-		ft_printf("%s\n", msg);
-	exit(ret);
+	mlx_destroy_window(mlx_id, mlx_win);
+	mlx_destroy_display(mlx_id);
+	free(mlx_id);
+}
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
 
 int	main(void)
 {
 	void	*mlx_id;
 	void	*mlx_win;
+	t_data	img;
 
 	mlx_id = mlx_init();
-	if (!mlx_id)
-		ft_exit(1, "Can't initialize mlx :(");
-	mlx_win = mlx_new_window(mlx_id, 500, 500, "KIKOOLOLMDR");
-	mlx_new_image(mlx_id, 500, 500);
-	if (!mlx_win)
-		ft_exit(1, "Can't open window :(");
+	mlx_win = mlx_new_window(mlx_id, WIDTH, HEIGHT, "KIKOOLOLMDR");
+	img.img = mlx_new_image(mlx_id, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx_id, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx_id);
+	destroy_and_free(mlx_id, mlx_win);
 	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,16 +6,28 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 08:44:47 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/02 09:37:27 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/02 13:40:13 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
-# include <math.h>
 # include "libft/libft.h"
 # include "mlx/mlx.h"
 # include "mlx/mlx_int.h"
+# include <math.h>
+
+# define WIDTH 500
+# define HEIGHT 500
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
 
 #endif
