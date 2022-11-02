5d4917147e66ae350e297583f11e9e7d25a82f52
zakissimo <zakaria.habri@gmail.com>
2022-11-02 12:07:35 +0100

    up

--- fractol.c
+++ fractol.c
@@ -6,17 +6,36 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 07:58:03 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/02 10:42:46 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/02 11:53:23 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+#include "libft/libft.h"
+#include "mlx/mlx.h"
+
+int		WIDTH = 500;
+int		LENGTH = 500;
+
+void	ft_exit(int ret, char *msg)
+{
+	if (msg)
+		ft_printf("%s\n", msg);
+	exit(ret);
+}
 
 int	main(void)
 {
 	void	*mlx_id;
+	void	*mlx_win;
 
 	mlx_id = mlx_init();
-	ft_printf("%p\n", mlx_id);
+	if (!mlx_id)
+		ft_exit(1, "Can't initialize mlx :(");
+	mlx_win = mlx_new_window(mlx_id, 500, 500, "KIKOOLOLMDR");
+	mlx_new_image(mlx_id, 500, 500);
+	if (!mlx_win)
+		ft_exit(1, "Can't open window :(");
+	mlx_loop(mlx_id);
 	return (0);
 }
