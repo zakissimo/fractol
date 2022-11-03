b9c6085ed43bb1f6b86d517f3ee7e0f105fd1ca8
zakissimo <zakaria.habri@gmail.com>
2022-11-03 08:41:36 +0100

    destroy and free

--- fractol.c
+++ fractol.c
@@ -6,7 +6,7 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 07:58:03 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/02 15:50:10 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/03 08:20:26 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 #include "libft/libft.h"
 #include "mlx/mlx.h"
 
-void	destroy_and_free(void *mlx_id, void *mlx_win)
+void	destroy_and_free(t_mlx mlx)
 {
-	mlx_destroy_window(mlx_id, mlx_win);
-	mlx_destroy_display(mlx_id);
-	free(mlx_id);
+	mlx_clear_window(mlx.ptr, mlx.win);
+	mlx_destroy_image(mlx.ptr, mlx.img->img);
+	mlx_destroy_window(mlx.ptr, mlx.win);
+	mlx_destroy_display(mlx.ptr);
+	free(mlx.ptr);
 }
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_image *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -31,18 +33,19 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 
 int	main(void)
 {
-	void	*mlx_id;
-	void	*mlx_win;
-	t_data	img;
+	t_mlx	mlx;
+	t_image	image;
 
-	mlx_id = mlx_init();
-	mlx_win = mlx_new_window(mlx_id, WIDTH, HEIGHT, "KIKOOLOLMDR");
-	img.img = mlx_new_image(mlx_id, WIDTH, HEIGHT);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-			&img.endian);
-	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-	mlx_put_image_to_window(mlx_id, mlx_win, img.img, 0, 0);
-	mlx_loop(mlx_id);
-	destroy_and_free(mlx_id, mlx_win);
+	mlx.ptr = mlx_init();
+	mlx.win = mlx_new_window(mlx.ptr, WIDTH, HEIGHT, "KIKOOLOLMDR");
+	image.img = mlx_new_image(mlx.ptr, WIDTH, HEIGHT);
+	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel,
+			&image.line_length, &image.endian);
+	my_mlx_pixel_put(&image, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx.ptr, mlx.win, image.img, 0, 0);
+	mlx.img = &image;
+	mlx_hook(mlx.win, 2, 1L << 0, close, &mlx);
+	mlx_loop(mlx.ptr);
+	destroy_and_free(mlx);
 	return (0);
 }
--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: zhabri <zhabri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/02 08:44:47 by zhabri            #+#    #+#             */
-/*   Updated: 2022/11/02 13:40:13 by zhabri           ###   ########.fr       */
+/*   Updated: 2022/11/03 08:17:54 by zhabri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,23 @@
 # include "mlx/mlx_int.h"
 # include <math.h>
 
-# define WIDTH 500
-# define HEIGHT 500
-
-typedef struct s_data
+typedef struct s_image
 {
 	void	*img;
 	char	*addr;
 	int		bits_per_pixel;
 	int		line_length;
 	int		endian;
-}			t_data;
+}			t_image;
+
+typedef struct s_mlx
+{
+	void	*ptr;
+	void	*win;
+	t_image	*img;
+}			t_mlx;
+
+# define WIDTH 500
+# define HEIGHT 500
 
 #endif
